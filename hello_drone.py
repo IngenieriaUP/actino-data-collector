cea0ff72d201533aedb95dfdd7f6421c93c1a359
Claudio Ortega <claudio.rtega2701@gmail.com>
2019-09-17 16:30:49 +0200

    get almost all vehicle attributes

--- hello_drone.py
+++ hello_drone.py
@@ -17,16 +17,28 @@ except:
     vehicle = connect(connection_string, wait_ready=True)
 
 print("Get some vehicle attributes (state):")
-print("""
-    GPS: {},
-    Battery: {},
-    Last Heartbeat: {},
-    Is Armable?: {},
-    System status: {},
-    Mode: {}
-""".format(vehicle.gps_0, vehicle.battery, vehicle.last_heartbeat,
-           vehicle.is_armable, vehicle.system_status.state, vehicle.mode.name)
-)
+print("Autopilot Firmware version: %s" % vehicle.version)
+print("Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp)
+print("Global Location: %s" % vehicle.location.global_frame)
+print("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print("Local Location: %s" % vehicle.location.local_frame)
+print("Attitude: %s" % vehicle.attitude)
+print("Velocity: %s" % vehicle.velocity)
+print("GPS: %s" % vehicle.gps_0)
+print("Groundspeed: %s" % vehicle.groundspeed)
+print("Airspeed: %s" % vehicle.airspeed)
+print("Gimbal status: %s" % vehicle.gimbal)
+print("Battery: %s" % vehicle.battery)
+print("EKF OK?: %s" % vehicle.ekf_ok)
+print("Last Heartbeat: %s" % vehicle.last_heartbeat)
+print("Rangefinder: %s" % vehicle.rangefinder)
+print("Rangefinder distance: %s" % vehicle.rangefinder.distance)
+print("Rangefinder voltage: %s" % vehicle.rangefinder.voltage)
+print("Heading: %s" % vehicle.heading)
+print("Is Armable?: %s" % vehicle.is_armable)
+print("System status: %s" % vehicle.system_status.state)
+print("Mode: %s" % vehicle.mode.name)
+print("Armed: %s" % vehicle.armed)
 
 vehicle.close()
 
@@ -34,27 +46,3 @@ if sitl:
     sitl.stop()
 
 print("Completed")
-
-# # vehicle attributes
-# print("Autopilot Firmware version: %s" % vehicle.version)
-# print("Autopilot capabilities (supports ftp): %s" % vehicle.capabilities.ftp)
-# print("Global Location: %s" % vehicle.location.global_frame)
-# print("Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
-# print("Local Location: %s" % vehicle.location.local_frame)
-# print("Attitude: %s" % vehicle.attitude)
-# print("Velocity: %s" % vehicle.velocity)
-# print("GPS: %s" % vehicle.gps_0)
-# print("Groundspeed: %s" % vehicle.groundspeed)
-# print("Airspeed: %s" % vehicle.airspeed)
-# print("Gimbal status: %s" % vehicle.gimbal)
-# print("Battery: %s" % vehicle.battery)
-# print("EKF OK?: %s" % vehicle.ekf_ok)
-# print("Last Heartbeat: %s" % vehicle.last_heartbeat)
-# print("Rangefinder: %s" % vehicle.rangefinder)
-# print("Rangefinder distance: %s" % vehicle.rangefinder.distance)
-# print("Rangefinder voltage: %s" % vehicle.rangefinder.voltage)
-# print("Heading: %s" % vehicle.heading)
-# print("Is Armable?: %s" % vehicle.is_armable)
-# print("System status: %s" % vehicle.system_status.state)
-# print("Mode: %s" % vehicle.mode.name)
-# print("Armed: %s" % vehicle.armed)
